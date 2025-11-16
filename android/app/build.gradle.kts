e377a6dfcecc2ef25d201d89e080f9952d3db2ba
Larissa Serres Espinosa <serresespinosalarissa@gmail.com>
2025-11-16 20:40:53 -0300

    'att'

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    id("com.google.gms.google-services")
+
 }
+
 
 android {
     namespace = "com.example.viva_mais"
@@ -41,4 +45,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 }
